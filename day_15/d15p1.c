4b89b1646d5c95164d5c631c59d91be5fa2ab7ed
thesivam124-creator <thesivam124@gmail.com>
2026-06-16 20:36:05 +0530

    day_15  assignment is uploaded

---
+++ day_15/d15p1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int start = 0, end = n - 1, temp;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    reverseArray(arr, n);
+
+    printf("Reversed Array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
